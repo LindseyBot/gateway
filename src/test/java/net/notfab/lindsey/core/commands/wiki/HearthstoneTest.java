f007cbd1a95c0ae32c517c6eb8f1448539b68c48
Felipe Schneider <ffelipeschneider@gmail.com>
2020-06-25 21:25:20 -0300

    fix hearthstone command

--- src/test/java/net/notfab/lindsey/core/commands/wiki/HearthstoneTest.java
+++ src/test/java/net/notfab/lindsey/core/commands/wiki/HearthstoneTest.java
@@ -27,7 +27,7 @@ class HearthstoneTest {
     @Test
     void getInfo() {
         CommandDescriptor info = command.getInfo();
-        assertEquals("hscard", info.getName(), "Name must be hscard");
+        assertEquals("hearthstone", info.getName(), "Name must be hearthstone");
         assertEquals(Modules.GAME_WIKI, info.getModule(), "Module must be game wiki");
         assertTrue(info.getPermissions().containsKey("commands." + info.getName()), "Must have permission with command name");
     }
@@ -35,7 +35,7 @@ class HearthstoneTest {
     @Test
     void help() {
         HelpArticle article = command.help(null);
-        HelpPage page = article.get("hscard");
+        HelpPage page = article.get("hearthstone");
         assertNotNull(page, "Help page must not be null");
         assertEquals("commands." + command.getInfo().getName(), page.getPermission(), "Permission must be command name");
     }
