b46e1a37e8f7a935b60c4315bc02c440a2bd7dd1
Felipe Schneider <ffelipeschneider@gmail.com>
2020-09-03 15:26:50 -0300

    Fix test issues

--- src/test/java/net/notfab/lindsey/core/commands/fun/MyAnimeListTest.java
+++ src/test/java/net/notfab/lindsey/core/commands/fun/MyAnimeListTest.java
@@ -29,7 +29,8 @@ class MyAnimeListTest {
         CommandDescriptor info = command.getInfo();
         assertEquals("myanimelist", info.getName(), "Name must be MyAnimeList");
         assertEquals(Modules.FUN, info.getModule(), "Module must be fun");
-        assertTrue(info.getPermissions().containsKey("commands." + info.getName()), "Must have permission with command name");
+        assertTrue(info.getPermissions().stream()
+                .anyMatch(perm -> perm.getName().equals("commands." + info.getName())), "Must have permission with command name");
     }
 
     @Test
