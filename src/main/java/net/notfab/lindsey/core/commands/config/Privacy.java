f840eb1efe49d242c89ca2a977cd1419d1972c10
Fabricio Winter <git@notfab.net>
2020-09-24 21:28:58 -0300

    Add privacy command

---
+++ src/main/java/net/notfab/lindsey/core/commands/config/Privacy.java
@@ -0,0 +1,53 @@
+package net.notfab.lindsey.core.commands.config;
+
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.notfab.lindsey.framework.command.Bundle;
+import net.notfab.lindsey.framework.command.Command;
+import net.notfab.lindsey.framework.command.CommandDescriptor;
+import net.notfab.lindsey.framework.command.Modules;
+import net.notfab.lindsey.framework.command.help.HelpArticle;
+import net.notfab.lindsey.framework.command.help.HelpPage;
+import net.notfab.lindsey.framework.i18n.Messenger;
+import net.notfab.lindsey.framework.i18n.Translator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Privacy implements Command {
+
+    @Autowired
+    private Translator i18n;
+
+    @Autowired
+    private Messenger msg;
+
+    @Override
+    public CommandDescriptor getInfo() {
+        return new CommandDescriptor.Builder()
+            .name("privacy")
+            .module(Modules.CORE)
+            .permission("commands.privacy", "permissions.command")
+            .build();
+    }
+
+    @Override
+    public boolean execute(Member member, TextChannel channel, String[] args, Message message, Bundle bundle) throws Exception {
+        msg.send(channel, sender(member) + i18n.get(member, "commands.core.privacy.link"));
+        return true;
+    }
+
+    @Override
+    public HelpArticle help(Member member) {
+        HelpPage page = new HelpPage("privacy")
+            .text("commands.core.privacy.description")
+            .usage("L!privacy")
+            .permission("commands.privacy")
+            .addExample("L!privacy");
+        return HelpArticle.of(page);
+    }
+
+}
