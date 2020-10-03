e1aa51b686028c7f13cb08828aa7418e192bccb6
Fabricio Winter <git@notfab.net>
2020-10-03 14:07:31 -0300

    Remove exception to allow for testing

--- src/main/java/net/notfab/lindsey/framework/leaderboard/LeaderboardService.java
+++ src/main/java/net/notfab/lindsey/framework/leaderboard/LeaderboardService.java
@@ -1,12 +1,14 @@
 package net.notfab.lindsey.framework.leaderboard;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class LeaderboardService {
 
     public void update(long user, LeaderboardType type) {
-        throw new RuntimeException("Not yet implemented");
+        log.info("Would have updated the leaderboard (u=" + user + ",t=" + type.name() + ")");
     }
 
 }
