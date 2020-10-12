0f0a5ea4be0819ac6a1c96ab9f4774062acada29
Felipe Schneider <ffelipeschneider@gmail.com>
2020-10-11 22:27:26 -0300

    add test

---
+++ src/test/java/net/notfab/lindsey/core/commands/wiki/PokedexTest.java
@@ -0,0 +1,44 @@
+package net.notfab.lindsey.core.commands.wiki;
+
+import net.notfab.lindsey.framework.command.CommandDescriptor;
+import net.notfab.lindsey.framework.command.Modules;
+import net.notfab.lindsey.framework.command.help.HelpArticle;
+import net.notfab.lindsey.framework.command.help.HelpPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class PokedexTest {
+
+    private Pokedex command;
+
+    @BeforeEach
+    void setUp() {
+        command = mock(Pokedex.class);
+        when(command.getInfo())
+                .thenCallRealMethod();
+        when(command.help(null))
+                .thenCallRealMethod();
+    }
+
+    @Test
+    void getInfo() {
+        CommandDescriptor info = command.getInfo();
+        assertEquals("pokedex", info.getName(), "Name must be pokedex");
+        assertEquals(Modules.GAME_WIKI, info.getModule(), "Module must be game wiki");
+        assertTrue(info.getPermissions().stream()
+                .anyMatch(perm -> perm.getName().equals("commands." + info.getName())), "Must have permission with command name");
+    }
+
+    @Test
+    void help() {
+        HelpArticle article = command.help(null);
+        HelpPage page = article.get("pokedex");
+        assertNotNull(page, "Help page must not be null");
+        assertEquals("commands." + command.getInfo().getName(), page.getPermission(), "Permission must be command name");
+    }
+
+}
