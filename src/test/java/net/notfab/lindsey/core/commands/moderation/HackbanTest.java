51c6e2381f768527463fa35dca9133fe19d7d019
Fabricio Winter <git@notfab.net>
2021-02-12 19:38:32 -0300

    Add hackban command
    Signed-off-by: Fabricio Winter <git@notfab.net>

---
+++ src/test/java/net/notfab/lindsey/core/commands/moderation/HackbanTest.java
@@ -0,0 +1,44 @@
+package net.notfab.lindsey.core.commands.moderation;
+
+import net.notfab.lindsey.core.framework.command.CommandDescriptor;
+import net.notfab.lindsey.core.framework.command.Modules;
+import net.notfab.lindsey.core.framework.command.help.HelpArticle;
+import net.notfab.lindsey.core.framework.command.help.HelpPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HackbanTest {
+
+    private Hackban command;
+
+    @BeforeEach
+    void setUp() {
+        command = mock(Hackban.class);
+        when(command.getInfo())
+                .thenCallRealMethod();
+        when(command.help(null))
+                .thenCallRealMethod();
+    }
+
+    @Test
+    void getInfo() {
+        CommandDescriptor info = command.getInfo();
+        assertEquals("hackban", info.getName(), "Name must be hackban");
+        assertEquals(Modules.MODERATION, info.getModule(), "Module must be Moderation");
+        assertTrue(info.getPermissions().stream()
+                .anyMatch(perm -> perm.getName().equals("commands." + info.getName())), "Must have permission with command name");
+    }
+
+    @Test
+    void help() {
+        HelpArticle article = command.help(null);
+        HelpPage page = article.get("hackban");
+        assertNotNull(page, "Help page must not be null");
+        assertEquals("commands." + command.getInfo().getName(), page.getPermission(), "Permission must be command name");
+    }
+
+}
