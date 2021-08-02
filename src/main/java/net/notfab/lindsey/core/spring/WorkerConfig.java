4bf16368676789b4edb6b757c9c022807df32e97
Fabricio Winter <git@notfab.net>
2021-08-01 23:22:24 -0300

    Remove duplicate event service
    Signed-off-by: Fabricio Winter <git@notfab.net>

--- src/main/java/net/notfab/lindsey/core/spring/WorkerConfig.java
+++ src/main/java/net/notfab/lindsey/core/spring/WorkerConfig.java
@@ -3,17 +3,11 @@ package net.notfab.lindsey.core.spring;
 import net.lindseybot.controller.registry.ButtonRegistry;
 import net.lindseybot.controller.registry.CommandRegistry;
 import net.lindseybot.properties.ControllerProperties;
-import net.notfab.lindsey.core.service.EventService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WorkerConfig {
-
-    @Bean
-    public EventService eventService() {
-        return new EventService();
-    }
 
     @Bean
     public CommandRegistry commands(ControllerProperties properties) {
