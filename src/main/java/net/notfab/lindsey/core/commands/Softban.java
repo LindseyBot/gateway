0c8958aac3f8a4d737a8e237a80b7b2a578f1f40
Fabricio Winter <git@notfab.net>
2021-09-12 09:56:06 -0300

    Add softban
    Signed-off-by: Fabricio Winter <git@notfab.net>

--- src/main/java/net/notfab/lindsey/core/commands/Softban.java
+++ src/main/java/net/notfab/lindsey/core/commands/Softban.java
@@ -1,5 +1,6 @@
 package net.notfab.lindsey.core.commands;
 
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.lindseybot.entities.discord.Label;
 import net.lindseybot.entities.interaction.commands.CommandMeta;
@@ -14,45 +15,53 @@ import net.notfab.lindsey.core.service.Messenger;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Hackban extends Command {
+public class Softban extends Command {
 
     private final Messenger msg;
     private final AuditService audit;
 
-    public Hackban(Messenger msg, AuditService audit) {
+    public Softban(Messenger msg, AuditService audit) {
         this.msg = msg;
         this.audit = audit;
     }
 
     @Override
     public CommandMeta getMetadata() {
-        return new CommandBuilder("hackban", Label.of("commands.hackban.description"), 1.0)
+        return new CommandBuilder("softban", Label.of("commands.softban.description"), 1.0)
             .module(Modules.MODERATION)
             .privileged()
             .guilds(859946655310413844L)
-            .addOption(OptType.LONG, "user", Label.of("commands.hackban.user"), true)
-            .addOption(OptType.STRING, "reason", Label.of("commands.hackban.reason"), false)
+            .addOption(OptType.MEMBER, "user", Label.of("commands.softban.user"), true)
+            .addOption(OptType.STRING, "reason", Label.of("commands.softban.reason"), false)
             .build();
     }
 
-    @BotCommand("hackban")
+    @BotCommand("softban")
     public void onCommand(ServerCommandEvent event) {
-        long target = event.getOptions().getLong("user");
+        Member member = event.getOptions().getMember("user");
+        if (member == null) {
+            this.msg.reply(event, Label.of("search.member"), true);
+            return;
+        }
         String reason;
         if (!event.getOptions().has("reason")) {
             reason = "Banned by " + this.getAsTag(event.getMember());
         } else {
             reason = event.getOptions().getString("reason");
         }
-        event.getGuild().ban(String.valueOf(target), 7, reason)
-            .queue((v) -> this.onBanned(event.getUnderlying(), target, reason));
-        this.msg.reply(event, Label.of("commands.hackban.banned"), true);
+        event.getGuild().ban(member, 7, reason).queue((v) -> {
+            this.onBanned(event.getUnderlying(), member, reason);
+            event.getGuild().unban(member.getId())
+                .reason(reason)
+                .queue();
+        });
+        this.msg.reply(event, Label.of("commands.softban.banned"), true);
     }
 
-    private void onBanned(SlashCommandEvent event, long target, String reason) {
+    private void onBanned(SlashCommandEvent event, Member target, String reason) {
         this.audit.builder().from(event)
-            .message("logs.hackban")
-            .field("target_id", target).field("target", "External User#000")
+            .message("logs.softban")
+            .target(target)
             .field("delDays", 7)
             .field("reason", reason)
             .send();
